d49f8f2d66df5bcd127b16a61e806f8647d8311f
silvakeci1 <silvakeci.ing@gmail.com>
2023-09-07 14:57:42 +0200

    place App

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native"
+import { Colors } from "../../constants/color";
+import ImagePicker from "./ImagePicker";
+import LocationPicker from "./LocationPicker";
+
+function PlaceForm() {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+
+    function changeTitleHandler(enteredText) {
+        setEnteredTitle(enteredText);
+    }
+
+    return (
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title </Text>
+                <TextInput style={styles.input} onChangeText={changeTitleHandler} value={enteredTitle} />
+            </View>
+            <ImagePicker />
+            <LocationPicker />
+        </ScrollView>
+    )
+
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({
+    form: {
+        flex: 1,
+        padding: 24
+    },
+    label: {
+        fontWeight: 'bold',
+        marginBottom: 4,
+        color: Colors.primary500
+    },
+    input: {
+        marginVertical: 8,
+        paddingHorizontal: 4,
+        paddingVertical: 8,
+        fontSize: 16,
+        borderBottomColor: Colors.primary700,
+        borderBottomWidth: 2,
+        backgroundColor: Colors.primary100
+    }
+})
---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,19 @@
+import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+
+function PlaceItem({place,  onSelect}) {
+    return (
+        <Pressable onPress={onSelect}>
+            <Image source={{uri: place.imageUri}} />
+            <View>
+                <Text>{place.title}</Text>
+                <Text>{place.address}</Text>
+            </View>
+        </Pressable>
+    )
+}
+
+export default PlaceItem;
+
+const styles = StyleSheet.create({
+
+})
